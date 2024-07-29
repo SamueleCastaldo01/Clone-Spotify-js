a664682f135d528148cc3efcb2e320142432665f
Zoltan3do <baronemanuel05@gmail.com>
2024-07-29 21:36:14 +0200

    indentation

--- assets/js/index.js
+++ assets/js/index.js
@@ -109,23 +109,23 @@ const albumData = function(album) {
             carouselRow.innerHTML += `
             <div class="carousel-item ${active}">
                 <div class="row p-3">
-                                    <div class="col-3 mt-3"><img src="${element.album.cover_medium}" alt="imgprova"
-                                            class="w-100"></div>
-                                    <div class="col-7">
-                                        <h6 class="fs-supersmall">ALBUM</h6>
-                                        <h1>${element.title_short}</h1>
-                                        <p class="fs-small">${element.album.title}</p>
-                                        <p class="fs-small mb-0">${convertDuration(element.duration)}</p>
-                                        <div class="w-100 d-flex align-items-center ">
-                                            <button class="btn btn-sm bg-primary rounded-5 px-4 py-2 me-3 h-25 fw-bold text-black">Play</button>
-                                            <button class="btn btn-sm bg-black text-white rounded-5 px-4 py-2 me-3 h-25 border border-white border-1">Salva</button>
-                                            <p class="fs-1">...</p>
-                                        </div>
-                                    </div>
-                                    <div class="col-2">
-                                      <button disabled class="btn text-gray2 bg-grayground fs-supersmall rounded-5  border-0">NASCONDI ANNUNCI</button>
-                                    </div>
-                                </div>
+                    <div class="col-3 mt-3"><img src="${element.album.cover_medium}" alt="imgprova"
+                        class="w-100"></div>
+                    <div class="col-7">
+                        <h6 class="fs-supersmall">ALBUM</h6>
+                        <h1>${element.title_short}</h1>
+                        <p class="fs-small">${element.album.title}</p>
+                        <p class="fs-small mb-0">${convertDuration(element.duration)}</p>
+                        <div class="w-100 d-flex align-items-center ">
+                            <button class="btn btn-sm bg-primary rounded-5 px-4 py-2 me-3 h-25 fw-bold text-black">Play</button>
+                            <button class="btn btn-sm bg-black text-white rounded-5 px-4 py-2 me-3 h-25 border border-white border-1">Salva</button>
+                            <p class="fs-1">...</p>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <button disabled class="btn text-gray2 bg-grayground fs-supersmall rounded-5  border-0">NASCONDI ANNUNCI</button>
+                    </div>
+                </div>
             </div>
         `;
             
