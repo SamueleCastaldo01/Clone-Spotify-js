dca189b363790ebb160f769bed7c1d3529d6d24d
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-07-29 18:36:03 +0200

    HomePage
    Co-authored-by: Manuel Barone <Zoltan3do@users.noreply.github.com>
    Co-authored-by: AlessioLosi <AlessioLosi@users.noreply.github.com>
    Co-authored-by: Nicolomecca <Nicolomecca@users.noreply.github.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 const albumData = function(album) {
     const apiKey = `https://striveschool-api.herokuapp.com/api/deezer/album/${album}`;
+    const carouselRow = document.getElementById('carousel');
     
     fetch(apiKey)
     .then((response) => {
@@ -11,35 +12,51 @@ const albumData = function(album) {
     })
     .then((dataAlbum) => {
         console.log('Data Album', dataAlbum);
-       const arrayAlbum = Array.from(dataAlbum)
-       arrayAlbum.forEach((album) => {
-            const newAlbum = `
-                        <div class="carousel-item active">
-                                <div class="row p-3">
-                                    <div class="col-3 mt-3"><img src="assets/imgs/search/image-10.jpg" alt="imgprova"
+        // const arrayAlbum = Array.from(dataAlbum)
+        const albumtracks = Array.from(dataAlbum.tracks.data);
+
+        // arrayAlbum.forEach((album) => {
+        albumtracks.forEach((element, i) => {
+            const active = i === 0 ? "active" : "";
+            carouselRow.innerHTML += `
+            <div class="carousel-item ${active}">
+                <div class="row p-3">
+                                    <div class="col-3 mt-3"><img src="${element.album.cover_medium}" alt="imgprova"
                                             class="w-100"></div>
                                     <div class="col-7">
                                         <h6 class="fs-supersmall">ALBUM</h6>
-                                        <h1>${dataAlbum.title}</h1>
-                                        <p class="fs-small">${dataAlbum.name}</p>
-                                        <p class="fs-small">${dataAlbum.duration}</p>
-                                        <div class="w-100 d-flex ">
-                                            <button class="btn bg-primary rounded-5 px-5 py-0 me-3">Play</button>
-                                            <button class="btn bg-black text-white rounded-5 px-5 py-0 me-3">Salva</button>
+                                        <h1>${element.title}</h1>
+                                        <p class="fs-small">${element.album.title}</p>
+                                        <p class="fs-small mb-0">${convertDuration(element.duration)}</p>
+                                        <div class="w-100 d-flex align-items-center ">
+                                            <button class="btn btn-sm bg-primary rounded-5 px-4 py-2 me-3 h-25 fw-bold text-black">Play</button>
+                                            <button class="btn btn-sm bg-black text-white rounded-5 px-4 py-2 me-3 h-25 border border-white border-1">Salva</button>
                                             <p class="fs-1">...</p>
                                         </div>
                                     </div>
                                     <div class="col-2">
-                                      <button disabled class="btn text-gray2 bg-grayground fs-supersmall rounded-5 border-0">NASCONDI ANNUNCI</button>
+                                      <button disabled class="btn text-gray2 bg-grayground fs-supersmall rounded-5  border-0">NASCONDI ANNUNCI</button>
                                     </div>
                                 </div>
-                            </div>
-            `
+            </div>
+        `;
+            
         });
+       
+
+        // });
+    
+
     })
     .catch((error) => {
         console.error('Errore:', error);
     });
 }
 
+
+const convertDuration = function(seconds){
+    const minutes = Math.floor(seconds/60)
+    const remainingSeconds = seconds %60
+    return `${minutes}:${remainingSeconds}`
+}
 albumData('75621062');
