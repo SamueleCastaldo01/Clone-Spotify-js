f8bd74cad12aa4fd79d46ab2f031e00e729e1de0
METAORI <nico97.mecca@gmail.com>
2024-07-30 14:48:38 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -154,12 +154,26 @@ function buildCarousel(datasetArray) {
 // Funzione per creare le card
 function createAlbumCards(albums) {
     cardsAlbumRow.innerHTML += `
+<<<<<<< Updated upstream
         <div class="col-3 mb-3"
           <div class="card w-25" ">
             <img src="${albums[0].album.cover_medium}" class="card-img-top" alt="img album">
             <div class="card-body ">
                 <h5 class="card-title"><a href = "album.html/${albums[0].album.id}">${albums[0].album.title}</a></h5>
                 <p class="card-text"><a href = "artist.html/${albums[0].artist.id}">${albums[0].artist.name}</a></p>
+=======
+        <div class="col-3 mb-3 scaleHover rounded"
+          <div class="card w-25 " >
+          <div class="position-relative">
+            <img src="${albums[0].album.cover_medium}" class="card-img-top rounded pt-2 " alt="img album">  
+            <button type="button" class="btn btn-primary circle-button position-absolute bottom-0 end-0 translate-middle d-none rounded-circle"><i class="bi bi-play-fill "></i></button>
+            </div>        
+            
+            <div class="card-body d-none">
+                <h5 class="card-title"><a href = "album.html/${albums[0].album.id}"  class="text-decoration-none text-white">${albums[0].album.title}</a></h5>
+                <p class="card-text mb-4 "><a href = "artist.html/${albums[0].artist.id}" class="text-decoration-none text-white">${albums[0].artist.name}</a></p>
+                     
+>>>>>>> Stashed changes
             </div>
         </div>`
 }
