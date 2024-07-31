b40516893d1601fd9e82c737fd3b13621ba1c187
METAORI <nico97.mecca@gmail.com>
2024-07-31 14:31:48 +0200

    alumjs

--- assets/js/albumdetails.js
+++ assets/js/albumdetails.js
@@ -19,14 +19,53 @@ fetch(keyUrl + albumId)
 function displayAlbumDetails(singleAlbum) {
     const albumDetailsContainer = document.getElementById('albumdettagli');
     albumDetailsContainer.innerHTML = `
-        <div class="singleAlbum-details">
-            <img src="${singleAlbum.cover_medium}" alt="Album Cover">
-            <h1>${singleAlbum.title}</h1>
-            <h2>${singleAlbum.artist.name}</h2>
-            <ul>
-                ${singleAlbum.tracks.data.map(track => `<li>${track.title_short} - ${convertDuration(track.duration)}</li>`).join('')}
-            </ul>
-        </div>`;
+    <div class="row  ">   
+                        <div class="d-flex mt-2 " >
+                        <i class="bi bi-arrow-left-circle fs-3"></i>
+                        <i class="bi bi-arrow-right-circle fs-3 ms-3"></i>
+                    </div>
+                        <div class="col-3 mt-3">
+                            <img src="${singleAlbum.cover_medium}"class="img-fluid" id="imgDetails"alt="img-album">
+                        </div>
+                        <div class="col-9 d-flex flex-column mt-2 ">
+                            <h3 class="mt-5" >Album</h3>
+                            <h1 class="flex-grow-2 mb-3">${singleAlbum.title}</h1>
+                            <p class="flex-grow-1 ">${singleAlbum.artist.name}</p>
+                        </div>
+
+                    </div>
+                    <div class="mt-5 d-flex align-items-center">
+                        <button type="button" class="btn btn-primary  rounded-circle me-2 "> <i class="bi bi-play-fill fs-4"></i></button>
+                        <i class="bi bi-heart ms-lg-3 fs-4"></i>
+                        <i class="bi bi-arrow-down-circle fs-4 ms-3"></i>
+                        <i class="bi bi-three-dots fs-4 ms-3"></i>
+
+
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-7 ">
+                            <ol>
+                                 <li>${singleAlbum.tracks.title_short}</li>
+                            </ol>
+
+                        </div>
+                        <div class="col-3">
+                            <ol>
+                                 
+                            </ol>
+
+
+                        </div>
+                        <div class="col-1 text-end">
+                            <ol>
+                            <li> ${convertDuration(singleAlbum.tracks.duration)}</li>
+                            </ol>
+
+
+                        </div>
+
+                    </div>
+    `;
 }
 
 function convertDuration(seconds) {
@@ -34,3 +73,4 @@ function convertDuration(seconds) {
     const remainingSeconds = seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60;
     return `${minutes}:${remainingSeconds}`;
 }
+
