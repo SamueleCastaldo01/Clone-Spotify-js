aa410000a1f91d6522fe1458563644ae04fa2fc7
AlessioLosi <alessiolosi93@icloud.com>
2024-08-01 17:42:35 +0200

    miPiacehtml

---
+++ assets/js/miPiace.js
@@ -0,0 +1,89 @@
+import { player, playerAlbumTrack, albumDataIni, playArtistFunction} from "./player.js";
+let likePlaylist=JSON.parse(localStorage.getItem('likePlaylist')) || [];
+console.log(likePlaylist)
+
+const playArtist = document.getElementById("playArtist");
+
+window.onload = function () {
+    albumDataIni("album", "11205422")
+    player();
+}
+
+playArtist.addEventListener("click", () => {
+    playArtistFunction()
+})
+
+
+
+    
+    function displayAlbumDetails(singleAlbum) {
+        const img = document.getElementById('imgDetails');
+        const titleAlbum = document.getElementById('title');
+        const artist = document.getElementById('artist');
+        const trackList = document.getElementById('lists'); 
+        const releaseYear = singleAlbum.release_date.split('-')[0];
+
+    
+        img.src = singleAlbum.cover_medium;
+        titleAlbum.innerText = truncate(singleAlbum.title,12);
+        artist.innerHTML = `<img src=" ${singleAlbum.artist.picture_small}" class="artist-img"> ${singleAlbum.artist.name.toUpperCase()} • ${releaseYear} • ${singleAlbum.nb_tracks} brani, <span class="text-min">${convertDurations(singleAlbum.duration)}</span>`;
+
+
+    
+        
+        trackList.innerHTML = '';
+    
+        
+        let trackHTML = ''; 
+        singleAlbum.tracks.data.forEach((track, index) => {
+            trackHTML += `
+                <div class="row" id="${track.id}">
+                    <div class="col-1 text-muted">
+                        ${index +1}
+                    </div>
+                    <div class="col-6" onclick='playerAlbumTrack(${track.id})'>
+                        <ol class="list-unstyled">
+                            <li class="title">${truncate(track.title_short,15)}</li>
+                            <a href="./artist.html?artistId=${track.artist.id}" class="no-underline text-undertitle text-decoration-none">${track.artist.name}</a>
+
+                        </ol>
+                    </div>
+                    <div class="col-4 ">
+                        <ol class="list-unstyled mb-0">
+                            <li class="text-muted">${track.rank}</li>
+                        </ol>
+                    </div>
+                    <div class="col-1 ">
+                        <ol class="list-unstyled mb-0">
+                            <li class="text-muted">${convertDuration(track.duration)}</li>
+                        </ol>
+                    </div>
+                </div>
+            `;
+        });
+        
+        trackList.innerHTML = trackHTML;
+    }
+
+function convertDuration(seconds) {
+    const minutes = Math.floor(seconds / 60) < 10 ? "0" + Math.floor(seconds / 60) : Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60;
+    return `${minutes}:${remainingSeconds}`;
+}
+
+function convertDurations(seconds) {
+    const minutes = Math.floor(seconds / 60) < 10 ? "0" + Math.floor(seconds / 60) : Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60;
+    return `${minutes} min ${remainingSeconds} sec`;
+}
+function truncate(text, maxLength) {
+    if (text.length > maxLength) {
+        return text.slice(0, maxLength) + '...';
+    }
+    return text;
+}
+
+
+window.playerAlbumTrack = playerAlbumTrack;
+window.albumDataIni = albumDataIni;
+window.playArtistFunction = playArtistFunction;
