16aa4b8935f2bf55d50aa877c4de5080eea3e2fa
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-01 11:31:14 +0200

    CommitAlbum

--- assets/js/albumdetails.js
+++ assets/js/albumdetails.js
@@ -31,18 +31,21 @@ fetch(keyUrl + albumId)
         const titleAlbum = document.getElementById('title');
         const artist = document.getElementById('artist');
         const trackList = document.getElementById('lists'); 
+        const releaseYear = singleAlbum.release_date.split('-')[0];
+
     
         img.src = singleAlbum.cover_medium;
-        titleAlbum.innerText = singleAlbum.title;
-        artist.innerText = singleAlbum.artist.name;
+        titleAlbum.innerText = truncate(singleAlbum.title,12);
+        artist.innerHTML = `<img src=" ${singleAlbum.artist.picture_small}" class="artist-img"> ${singleAlbum.artist.name.toUpperCase()} • ${releaseYear} • ${singleAlbum.nb_tracks} brani, <span class="text-min">${convertDurations(singleAlbum.duration)}</span>`;
+
+
     
         
         trackList.innerHTML = '';
     
         
         let trackHTML = ''; 
-    
-        singleAlbum.tracks.data.forEach((track) => {
+        singleAlbum.tracks.data.forEach((track, index) => {
             trackHTML += `
                 <div class="row" id="${track.id}">
                     <div class="col-8" onclick='playerAlbumTrack(${track.id})'>
@@ -51,20 +54,19 @@ fetch(keyUrl + albumId)
                             <p class="text-undertitle">${track.artist.name}</p>
                         </ol>
                     </div>
-                    <div class="col-3 ">
-                        <ol class="list-unstyled" >
-                            <li class="text-undertitle"> ${track.rank}</li> 
+                    <div class="col-3">
+                        <ol class="list-unstyled mb-0">
+                            <li class="text-muted">${track.rank}</li>
                         </ol>
                     </div>
                     <div class="col-1 text-end">
-                        <ol class="list-unstyled">  
-                            <li class="text-undertitle">${convertDuration(track.duration)}</li>
+                        <ol class="list-unstyled mb-0">
+                            <li class="text-muted">${convertDuration(track.duration)}</li>
                         </ol>
                     </div>
                 </div>
             `;
         });
-    
         
         trackList.innerHTML = trackHTML;
     }
@@ -73,6 +75,18 @@ function convertDuration(seconds) {
     const minutes = Math.floor(seconds / 60) < 10 ? "0" + Math.floor(seconds / 60) : Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60;
     return `${minutes}:${remainingSeconds}`;
+}
+
+function convertDurations(seconds) {
+    const minutes = Math.floor(seconds / 60) < 10 ? "0" + Math.floor(seconds / 60) : Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60;
+    return `${minutes} min ${remainingSeconds} sec`;
+}
+function truncate(text, maxLength) {
+    if (text.length > maxLength) {
+        return text.slice(0, maxLength) + '...';
+    }
+    return text;
 }
 
 
